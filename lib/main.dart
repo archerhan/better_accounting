a4c5360d8ef6aabae9d0bd33bc3601fc49447069
archerhan <archerhan1990@outlook.com>
2023-06-06 19:50:24 +0800

    Get框架初始化配置

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
+import 'package:better_accounting/constants/setting_service.dart';
 import 'package:better_accounting/i18n/app_translation.dart';
 import 'package:better_accounting/routes/app_pages.dart';
+import 'package:better_accounting/services/isar_service.dart';
 import 'package:bot_toast/bot_toast.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:get/route_manager.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
 
-void main() {
+void main() async {
+  await initServices();
   runApp(const MyApp());
+}
+
+/// service里面做App启动前的初始化,比如初始化存储,初始化主题数据,
+/// 初始化多语言(动态下发多语言),初始化设置等
+Future initServices() async {
+  await Get.putAsync(() async => await GetStorage.init());
+  await Get.putAsync(() => IsarService.instance.init());
+  await Get.putAsync(() => SettingService().init());
 }
 
 class MyApp extends StatelessWidget {
@@ -22,8 +34,11 @@ class MyApp extends StatelessWidget {
         splitScreenMode: true,
         builder: (context, child) {
           return GetMaterialApp(
+            enableLog: false,
             translationsKeys: AppTranslation.translations,
-            locale: const Locale('zh', 'CN'),
+            locale: Get.deviceLocale,
+            defaultTransition: Transition.cupertino,
+            popGesture: Get.isPopGestureEnable,
             fallbackLocale: const Locale('zh', 'CN'),
             initialRoute: AppPages.INITIAL,
             getPages: AppPages.routes,
--- lib/services/isar_service.dart
+++ lib/services/isar_service.dart
@@ -1,10 +1,11 @@
+import 'package:get/get.dart';
 import 'package:isar/isar.dart';
 import 'package:path_provider/path_provider.dart';
 
-class IsarHelper {
-  IsarHelper._privateConstructor();
-  static final IsarHelper _instance = IsarHelper._privateConstructor();
-  static IsarHelper get instance => _instance;
+class IsarService extends GetxService {
+  IsarService._privateConstructor();
+  static final IsarService _instance = IsarService._privateConstructor();
+  static IsarService get instance => _instance;
 
   late Isar isarInstance;
 
