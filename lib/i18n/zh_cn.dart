f7c0c89d75da0704272988ccb0931190a56466cf
archerhan <archerhan1990@outlook.com>
2023-06-08 16:23:41 +0800

    优化日期选择器

--- lib/i18n/zh_cn.dart
+++ lib/i18n/zh_cn.dart
@@ -6,5 +6,9 @@ class ZHCN {
     "common_day": "日",
     "common_ok": "确定",
     "common_cancel": "取消",
+    // 近期记账
+    "accounts_expenses":"支出",
+    "accounts_income": "收入",
+    "accounts_more_analyze": "更多分析"
   };
 }
